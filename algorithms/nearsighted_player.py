80af256635e9ddadcc940834c15449b208f01640
Paweł <pawel.pycia29@gmail.com>
2021-12-09 23:16:13 +0100

    Refractor code

--- algorithms/nearsighted_player.py
+++ algorithms/nearsighted_player.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 from base.action import Action
 from base.bot import Bot
@@ -9,18 +9,29 @@ from random import choice
 class NearSighted(Bot):
     def choose_action(self, state: State) -> Optional[Action]:
 
+        reasonable_actions, winning_action = self.get_actions(state)
+
+        if winning_action is not None:
+            return winning_action
+        if len(reasonable_actions) > 0:
+            return choice(reasonable_actions)
+
+        # every action leads to the defeat
+        return choice(self.game.actions_for(state, is_opponent=False))
+
+    def get_actions(self, state: State) -> Tuple[List[Optional[Action]], Optional[Action]]:
         reasonable_actions = []
+        winning_actions = None
         for player_action in self.game.actions_for(state, is_opponent=False):
             next_state = self.game.take_action(state, player_action)
             if not self.game.is_terminal_state(next_state):
-
                 is_valid = True
-                for opponent_action in self.game.actions_for(state, is_opponent=True):
-                    opponent_state = self.game.take_action(next_state, opponent_action)
-                    is_terminal = self.game.is_terminal(opponent_state)
+                for opponent_action in self.game.actions_for(next_state, is_opponent=True):
+                    second_state = self.game.take_action(next_state, opponent_action)
+                    is_terminal = self.game.is_terminal_state(second_state)
 
                     # there's a wining opportunity for opponent
-                    if is_terminal and self.game.value_for_terminal(opponent_state) < 0:
+                    if is_terminal and self.game.value_for_terminal(second_state) < 0:
                         is_valid = False
                         break
 
@@ -29,12 +40,6 @@ class NearSighted(Bot):
 
             else:
                 if self.game.value_for_terminal(next_state) > 0:
-                    return player_action
+                    winning_actions = player_action
 
-        #
-        if len(reasonable_actions) > 0: return choice(reasonable_actions)
-
-        # every action leads to the defeat
-        return choice(self.game.actions_for(state, is_opponent=False))
-
-
+        return reasonable_actions, winning_actions
