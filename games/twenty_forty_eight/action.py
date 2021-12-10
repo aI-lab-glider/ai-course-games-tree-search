12083fc281bc42ed510c088a2ac8688f0863c7fe
hankaj <hanna.jarlaczynska@gmail.com>
2021-12-10 14:34:28 +0100

    Add 2048 game

---
+++ games/twenty_forty_eight/action.py
@@ -0,0 +1,79 @@
+from enum import Enum, auto
+from dataclasses import dataclass
+from base.action import Action
+from copy import deepcopy
+from games.twenty_forty_eight.state import TwentyFortyEightState
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Direction(Enum):
+    RIGHT = auto()
+    LEFT = auto()
+    UP = auto()
+    DOWN = auto()
+
+
+@dataclass
+class TwentyFortyEightPlayerAction(Action):
+    direction: Direction
+
+    def apply(self, state: TwentyFortyEightState) -> TwentyFortyEightState:
+        moves = {
+            Direction.LEFT: self._move_left,
+            Direction.RIGHT: self._move_right,
+            Direction.UP: self._move_up,
+            Direction.DOWN: self._move_down
+        }
+        new_board = moves[self.direction](deepcopy(state.board))
+        return TwentyFortyEightState(board=new_board)
+
+    def _move_left(self, board: NDArray) -> NDArray:
+        board = self._compress(board)
+        board = self._merge(board)
+        return self._compress(board)
+
+    def _move_right(self, board: NDArray) -> NDArray:
+        board = self._move_left(np.flip(board, axis=1))
+        return np.flip(board, axis=1)
+
+    def _move_up(self, board: NDArray) -> NDArray:
+        new_board = self._move_left(board.transpose()).transpose()
+        return new_board
+
+    def _move_down(self, board: NDArray) -> NDArray:
+        new_board = self._move_right(board.transpose()).transpose()
+        return new_board
+
+    def _compress(self, board: NDArray) -> NDArray:
+        compressed_board = np.full((4, 4), 0)
+        for row in range(4):
+            compressed_row = np.array([block_value for block_value in board[row] if block_value != 0])
+            compressed_board[row, 0:compressed_row.size] = compressed_row
+        return compressed_board
+
+    def _merge(self, board: NDArray) -> NDArray:
+        for row in range(4):
+            for col in range(3):
+                if board[row][col] == board[row][col + 1] and board[row][col] != 0:
+                    board[row][col] *= 2
+                    board[row][col + 1] = 0
+        return board
+
+    def __hash__(self):
+        return hash(self.direction)
+
+
+@dataclass
+class TwentyFortyEightOpponentAction(Action):
+    row: int
+    col: int
+    block_value: int
+
+    def apply(self, state: TwentyFortyEightState) -> TwentyFortyEightState:
+        new_board = deepcopy(state.board)
+        new_board[self.row, self.col] = self.block_value
+        return TwentyFortyEightState(board=new_board)
+
+    def __hash__(self):
+        return hash((self.row, self.col, self.block_value))
---
+++ games/twenty_forty_eight/game.py
@@ -0,0 +1,76 @@
+from typing import List, Union
+from games.twenty_forty_eight.state import TwentyFortyEightState
+from games.twenty_forty_eight.action import TwentyFortyEightPlayerAction, TwentyFortyEightOpponentAction, Direction
+from base.game import Game
+import numpy as np
+import random
+
+
+class TwentyFortyEightGame(Game):
+    def __init__(self):
+        initial_state = self.initial_game_state()
+        super().__init__(initial_state)
+
+    def initial_game_state(self) -> TwentyFortyEightState:
+        state = TwentyFortyEightState(board=np.full((4, 4), 0))
+        for _ in range(2):
+            action = random.choice(self.actions_for(state, is_opponent=True))
+            state = self.take_action(state, action)
+        return state
+
+    def actions_for(self, from_state: TwentyFortyEightState, is_opponent: bool) -> List[
+            Union[TwentyFortyEightPlayerAction, TwentyFortyEightOpponentAction]]:
+        if is_opponent:
+            return [TwentyFortyEightOpponentAction(row, col, block_value=random.choices([2, 4], weights=[0.9, 0.1])[0])
+                    for row in range(4) for col in range(4)
+                    if from_state.board[row, col] == 0]
+        else:
+            return [TwentyFortyEightPlayerAction(direction) for direction in Direction if self._is_valid_move(direction, from_state)]
+
+    def _is_able_to_merge_row(self, state: TwentyFortyEightState) -> bool:
+        return any(state.board[row][col] == state.board[row][col + 1] and state.board[row][col] != 0
+                   for col in range(3) for row in range(4))
+
+    def _is_able_to_merge_col(self, state: TwentyFortyEightState) -> bool:
+        return any(state.board[row][col] == state.board[row + 1][col] and state.board[row][col] != 0
+                   for row in range(3) for col in range(4))
+
+    def _is_able_to_move_left(self, state: TwentyFortyEightState) -> bool:
+        return any(state.board[row, col] == 0 and state.board[row, col+1] != 0 for col in range(3) for row in range(4))
+
+    def _is_able_to_move_right(self, state: TwentyFortyEightState) -> bool:
+        return any(state.board[row, col] != 0 and state.board[row, col+1] == 0 for col in range(3) for row in range(4))
+
+    def _is_able_to_move_up(self, state: TwentyFortyEightState) -> bool:
+        return any(state.board[row, col] == 0 and state.board[row+1, col] != 0 for row in range(3) for col in range(4))
+
+    def _is_able_to_move_down(self, state: TwentyFortyEightState) -> bool:
+        return any(state.board[row, col] != 0 and state.board[row+1, col] == 0 for row in range(3) for col in range(4))
+
+    def _is_valid_move(self, direction: Direction, state: TwentyFortyEightState) -> bool:
+        validate_move = {
+            Direction.LEFT: (self._is_able_to_merge_row, self._is_able_to_move_left),
+            Direction.RIGHT: (self._is_able_to_merge_row, self._is_able_to_move_right),
+            Direction.UP: (self._is_able_to_merge_col, self._is_able_to_move_up),
+            Direction.DOWN: (self._is_able_to_merge_col, self._is_able_to_move_down)
+        }
+        if validate_move[direction][0](state):
+            return True
+        return validate_move[direction][1](state)
+
+    def take_action(self, state: TwentyFortyEightState,
+                    action: Union[TwentyFortyEightPlayerAction, TwentyFortyEightOpponentAction]) -> TwentyFortyEightState:
+        return action.apply(state)
+
+    def value_for_terminal(self, state: TwentyFortyEightState) -> float:
+        if 2048 in state.board:
+            return 1
+        return -1
+
+    def is_terminal_state(self, state: TwentyFortyEightState) -> bool:
+        if 2048 in state.board:
+            return True
+        return not(0 in state.board or self._is_able_to_merge_row(state) or self._is_able_to_merge_col(state))
+
+    def switch_players(self):
+        pass
