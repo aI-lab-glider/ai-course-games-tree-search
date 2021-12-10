0b1f0e5280041b392dabcd193cb910f2d94ae4f5
hankaj <hanna.jarlaczynska@gmail.com>
2021-12-10 14:36:43 +0100

    Change random bot to be able to play as the opponent

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from games.tictactoe.game import TicTacToeGame
+from games.twenty_forty_eight.game import TwentyFortyEightGame
 from algorithms.minimax import Minimax
+from algorithms.minimax_alpha_beta import MinimaxAlphaBeta
+from algorithms.monte_carlo import MonteCarlo
+from algorithms.random_player import RandomPlayer
 from match import Match
 
 
 if __name__ == "__main__":
-    game = TicTacToeGame()
-    player_a = Minimax(game)
-    player_b = Minimax(game.switch_players())
+    game = TwentyFortyEightGame()
+    player_a = MonteCarlo(game, n_rollouts=20)
+    player_b = RandomPlayer(game, is_opponent=True)
     match = Match(game, player_a, player_b)
     match.play()
