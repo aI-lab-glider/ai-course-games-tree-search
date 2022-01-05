916a93ab653468d5927c021f382759fe7f5058d5
czyjtu <maciejczyjtu@gmail.com>
2022-01-05 19:04:46 +0100

    mcts almost done

---
+++ algorithms/montec_carlo_tree_search.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+from base.bot import Bot, G
+from base.state import State
+from base.action import Action
+import math
+from typing import Optional, List
+from dataclasses import dataclass, field
+import random
+
+
+@dataclass
+class Node:
+    wins: int = 0 
+    visits: int = 0 
+    game_state: State = None
+    parent: Node = None
+    action: Action = None
+    children: List[Node] = field(default_factory=list)
+
+
+class MCTS(Bot):
+    def __init__(self, game: G, n_rollouts=1000):
+        super().__init__(game)
+        self.n_rollouts = n_rollouts
+
+
+    def select(self, node: Node) -> Node:
+        return node if not node.children else self.select(max(node.children, key=self.ucb))
+
+
+    def expand(self, node: Node):
+        if self.game.is_terminal_state(node.game_state):
+            return node 
+        node.children = [
+            Node(parent=node, game_state=self.game.take_action(node.game_state, a), action=a)
+            for a in self.game.actions_for(node.game_state) # TODO: pass 'is_oponent' parameter
+        ]
+        return random.choice(node.children)
+
+
+    def playout(self,  current_state: State) -> float:
+        is_opponent = True
+        while not self.game.is_terminal_state(current_state):
+            action = random.choice(self.game.actions_for(current_state, is_opponent))
+            current_state = self.game.take_action(current_state, action)
+            is_opponent = not is_opponent
+        return current_state, self.game.reward(current_state)
+
+    
+    def propagate(self, reward: float, node: Node) -> None:
+        if reward > 0:
+            node.wins += reward 
+            node.visits += 1 
+        self.propagate(-reward, node.parent)
+
+    
+    def choose_action(self, state: State) -> Optional[Action]:
+        root = Node(game_state=state)
+        for _ in range(self.n_rollouts):
+            expanded_node = self.expand(self.select(root))
+            _, reward = self.playout(expanded_node)
+            self.propagate(reward, expanded_node)
+        return max(root.children, key=lambda n: n.visits).action
+
+    
+    @staticmethod
+    def ucb(node: Node, c=1.4) -> float:
+        return float('inf') if node.visits == 0 else node.wins / node.visits + c * math.sqrt(math.log(node.parent.visits) / node.visits)
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from algorithms.minimax import Minimax
 from algorithms.minimax_alpha_beta import MinimaxAlphaBeta
 from algorithms.monte_carlo import MonteCarlo
 from algorithms.random_player import RandomPlayer
+from algorithms.montec_carlo_tree_search import MCTS
 from match import Match
 
 
 if __name__ == "__main__":
-    game = TicTacToeGame()
-    player_a = RandomPlayer(game) 
-    player_b = MonteCarlo(game.switch_players(), 100)
+    game = TwentyFortyEightGame()
+    player_a = MCTS(game, n_rollouts=20)
+    player_b = RandomPlayer(game, is_opponent=True)
     match = Match(game, player_a, player_b)
     match.play()
     match.to_gif()
